e917738a72c839a02d7f28bcffc9194b3fbf38d5
Rdataman <tclwhu@gmail.com>
2014-04-25 21:22:28 +0300

    Create cachematrix.R
    A few more comments on what to do

---
+++ cachematrix.R
@@ -0,0 +1,29 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
+
+    Status
+    API
+    Training
+    Shop
+    Blog
+    About
+
+    © 2014 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Contact
+
